26624164ccf9d383f9770805a849287ca5ad12cd
MaagadJM <139199115+MaagadJM@users.noreply.github.com>
2024-03-17 20:20:12 +0800

    Added google maps on footer

--- src/components/inc/Footer.jsx
+++ src/components/inc/Footer.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
+
 function Footer() {
   return (
     <section className="section footer bg-dark text-white">
@@ -50,6 +52,21 @@ function Footer() {
             <div>
               <p className="text-white mb-1">jude11318@gmail.com</p>
             </div>
+          </div>
+        </div>
+
+        {/* Google Maps iframe */}
+        <div className="row">
+          <div className="col">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d19434.68447561604!2d120.82993000588208!3d14.862821500948954!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sph!4v1710677133536!5m2!1sen!2sph"
+              width="600"
+              height="450"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
           </div>
         </div>
       </div>
