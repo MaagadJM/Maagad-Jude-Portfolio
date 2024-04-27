ee0c6140286829b8e5b936e4c64264328107b7a0
MaagadJM <139199115+MaagadJM@users.noreply.github.com>
2024-04-27 14:59:13 +0800

    Arranged the the 4 tabs inside of each folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./App.css";
-import Home from "./components/pages/Home";
-import Aboutme from "./components/pages/About";
-import Contactme from "./components/pages/Contact";
-import Projects from "./components/pages/Projects";
+import Home from "./components/pages/Home/Home";
+import Aboutme from "./components/pages/AboutMe/About";
+import Contactme from "./components/pages/MyContacts/Contact";
+import Projects from "./components/pages/MyProjects/Projects";
 import Navbar from "./components/inc/Navbar";
 import { BrowserRouter as HashRouter, Routes, Route } from "react-router-dom";
 import Footer from "./components/inc/Footer";
--- src/components/pages/AboutMe/About.jsx
+++ src/components/pages/AboutMe/About.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import VMC from "./inc/Vmc";
+import VMC from "../inc/Vmc";
 
 function Aboutme() {
   return (
