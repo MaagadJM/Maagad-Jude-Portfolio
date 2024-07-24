d82ef5ac419a3ca1e07be994e3b281b7393a9935
Jude Michael Maagad <jude11318@gmail.com>
2024-07-24 18:30:03 +0800

    fixed google map size and footer links

--- src/components/inc/Footer.jsx
+++ src/components/inc/Footer.jsx
@@ -4,10 +4,11 @@ import { Link } from "react-router-dom";
 function Footer() {
   return (
     <>
-      <section className="section footer bg-[#18181b] text-white py-4">
-        <div className="container">
-          <div className="row">
-            {/* <div className="col-md-4">
+      <main className="bg-[#18181b] pt-40">
+        <section className="footer text-white py-4">
+          <div className="mx-[15%]">
+            <div className="row">
+              {/* <div className="col-md-4">
               <h6>Company Information</h6>
               <hr />
               <p className="text-white">
@@ -17,59 +18,67 @@ function Footer() {
                 type specimen book.{" "}
               </p>
             </div> */}
-            <div className="col-md-6">
-              <h6>Quick Links</h6>
-              <hr />
-              <div>
-                <Link to="/">Home</Link>
+              <div className="col-md-6 font-light text-xl">
+                <p>Quick Links</p>
+                <hr className="text-[#ca8b04]" />
+                <div>
+                  <Link to="/">Home</Link>
+                </div>
+                <div>
+                  <Link to="/about">About</Link>
+                </div>
+                <div>
+                  <Link to="/contact">Contact</Link>
+                </div>
+                <div>
+                  <Link to="/projects">Projects</Link>
+                </div>
               </div>
-              <div>
-                <Link to="/about">About</Link>
-              </div>
-              <div>
-                <Link to="/contact">Contact</Link>
-              </div>
-              <div>
-                <Link to="/projects">Projects</Link>
-              </div>
-            </div>
 
-            <div className="col-md-6">
-              <h6>Contact Information</h6>
-              <hr />
-              <div>
-                <p className="text-white mb-1">
-                  Mag-Asawang Sapa, Santa Maria, Bulacan, Philippines
-                </p>
-              </div>
-              <div>
-                <p className="text-white mb-1">Jude Michael Daroya Maagad</p>
-              </div>
-              <div>
-                <p className="text-white mb-1">09217105661</p>
-              </div>
-              <div>
-                <p className="text-white mb-1">jude11318@gmail.com</p>
+              <div className="col-md-6 font-light text-xl">
+                <p>Contact Information</p>
+                <hr className="text-[#ca8b04]" />
+                <div>
+                  <p className="text-white mb-1">
+                    Mag-Asawang Sapa, Santa Maria, Bulacan, Philippines
+                  </p>
+                </div>
+                <div>
+                  <p className="text-white mb-1">Jude Michael Daroya Maagad</p>
+                </div>
+                <div>
+                  <p className="text-white mb-1">09217105661</p>
+                </div>
+                <div>
+                  <p className="text-white mb-1">jude11318@gmail.com</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <div className="container-fluid bg-[#212529]">
-        <div className="row pb-3">
-          <iframe
-    
+        <div className="py-10 mx-[15%]">
+          {/* <iframe
             src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d19434.68447561604!2d120.82993000588208!3d14.862821500948954!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sph!4v1710677133536!5m2!1sen!2sph"
             width="100%"
-            height="650"
-            style={{ border: 0, borderRadius: '40px' }}
+            height="150"
+            style={{ border: 0, borderRadius: "10px" }}
             allowFullScreen=""
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
+          ></iframe> */}
+
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d61707.475463848394!2d120.94566310141343!3d14.841120701582012!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397ac22ac27a55f%3A0x4a8611643c5c0dd0!2sSanta%20Maria%2C%20Bulacan!5e0!3m2!1sen!2sph!4v1721816779438!5m2!1sen!2sph"
+            width="100%"
+            height="150"
+            style={{ border: 0, borderRadius: "10px" }}
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
-      </div>
+      </main>
     </>
   );
 }
