c0885ab7baea1de2fe2c9266f10010b9759fb786
Jude Maagad <jude11318@gmail.com>
2025-09-18 15:35:11 +0800

    fix: adjustend imagePath for carousel icons

--- src/components/pages/Carousel/feicons.js
+++ src/components/pages/Carousel/feicons.js
@@ -1,142 +1,144 @@
 export const feicons = [
   {
     id: 1,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/html5.svg",
+    // imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/html5.svg",
+    imagePath: "/CarouselIcons/html5.svg",
   },
   {
     id: 2,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/css.svg",
+    imagePath: "/CarouselIcons/css.svg",
   },
   {
     id: 3,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/js.svg",
+    imagePath: "/CarouselIcons/js.svg",
   },
   {
     id: 4,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/sass.svg",
+    imagePath: "/CarouselIcons/sass.svg",
   },
   {
     id: 5,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/bootstrap.svg",
+    imagePath: "/CarouselIcons/bootstrap.svg",
   },
   {
     id: 6,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/chakraui.svg",
+    imagePath: "/CarouselIcons/chakraui.svg",
   },
   {
     id: 7,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/tailwindcss.svg",
+    imagePath: "/CarouselIcons/tailwindcss.svg",
     src: "https://www.facebook.com/",
   },
   {
     id: 8,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/react.svg",
+    imagePath: "/CarouselIcons/react.svg",
   },
   {
     id: 9,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/nodejs.svg",
+    imagePath: "/CarouselIcons/nodejs.svg",
   },
   {
     id: 10,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/git.svg",
+    imagePath: "/CarouselIcons/git.svg",
   },
   {
     id: 11,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/github.svg",
+    imagePath: "/CarouselIcons/github.svg",
   },
   {
     id: 12,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/bitbucket.svg",
+    imagePath: "/CarouselIcons/bitbucket.svg",
   },
   {
     id: 13,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/jira.svg",
+    imagePath: "/CarouselIcons/jira.svg",
   },
   {
     id: 14,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/blender.svg",
+    imagePath: "/CarouselIcons/blender.svg",
   },
   {
     id: 15,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/figma.svg",
+    imagePath: "/CarouselIcons/figma.svg",
   },
   {
     id: 16,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/gatsbyjs.svg",
+    imagePath: "/CarouselIcons/gatsbyjs.svg",
   },
   {
     id: 17,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/graphql.svg",
+    imagePath: "/CarouselIcons/graphql.svg",
   },
 
   // ============== DUPLICATE FOR SMOOTHER CAROUSEL =============== //
 
   {
     id: 18,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/html5.svg",
+    // imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/html5.svg",
+    imagePath: "/CarouselIcons/html5.svg",
   },
   {
     id: 19,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/css.svg",
+    imagePath: "/CarouselIcons/css.svg",
   },
   {
     id: 20,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/js.svg",
+    imagePath: "/CarouselIcons/js.svg",
   },
   {
     id: 21,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/sass.svg",
+    imagePath: "/CarouselIcons/sass.svg",
   },
   {
     id: 22,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/bootstrap.svg",
+    imagePath: "/CarouselIcons/bootstrap.svg",
   },
   {
     id: 23,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/chakraui.svg",
+    imagePath: "/CarouselIcons/chakraui.svg",
   },
   {
     id: 24,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/tailwindcss.svg",
+    imagePath: "/CarouselIcons/tailwindcss.svg",
   },
   {
     id: 25,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/react.svg",
+    imagePath: "/CarouselIcons/react.svg",
   },
   {
     id: 26,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/nodejs.svg",
+    imagePath: "/CarouselIcons/nodejs.svg",
   },
   {
     id: 27,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/git.svg",
+    imagePath: "/CarouselIcons/git.svg",
   },
   {
     id: 28,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/github.svg",
+    imagePath: "/CarouselIcons/github.svg",
   },
   {
     id: 29,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/bitbucket.svg",
+    imagePath: "/CarouselIcons/bitbucket.svg",
   },
   {
     id: 30,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/jira.svg",
+    imagePath: "/CarouselIcons/jira.svg",
   },
   {
     id: 31,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/blender.svg",
+    imagePath: "/CarouselIcons/blender.svg",
   },
   {
     id: 32,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/figma.svg",
+    imagePath: "/CarouselIcons/figma.svg",
   },
   {
     id: 33,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/gatsbyjs.svg",
+    imagePath: "/CarouselIcons/gatsbyjs.svg",
   },
   {
     id: 34,
-    imagePath: "/Maagad-Jude-Portfolio/CarouselIcons/graphql.svg",
+    imagePath: "/CarouselIcons/graphql.svg",
   },
 ];
